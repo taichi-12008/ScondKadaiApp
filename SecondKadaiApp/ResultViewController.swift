c494517a51234eecb908c9fc3427f916bfdda299
taichi.imaoka <taichi0305.12008@gmail.com>
2020-12-31 21:29:05 +0900

    課題制作完了

---
+++ SecondKadaiApp/ResultViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 今岡大智 on 2020/12/30.
+//  Copyright © 2020 taichi.imaoka. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    var x = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        let yourName = x
+        label.text = "\(yourName)"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -9,10 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+//    StoryboardのUI部品と変数を関連付け
+    @IBOutlet weak var textFiled: UITextField!
+    
+    var textFiledString:String! = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        //他の画面からsegueを使って戻ってきた時の処理
+    }
+    
+    @IBAction func button(_ sender: Any) {
+        //遷移ボタンを押した時の処理
+        textFiledString = textFiled.text!
+    }
+    
+    
+//    遷移するときに呼ばれるprepareメソッドを追加
+//    遷移するときに行う処理をこの中に記載する
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // segueから遷移先のResultViewControllerを取得する
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        
+        resultViewController.x = textFiledString
+        
     }
 
 
